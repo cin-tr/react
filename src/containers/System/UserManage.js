077c734459d71418e556910a823d783e405e723c
hoaitrinh <hoaitrinh2106@gmail.com>
2024-03-09 16:56:31 +0700

    CRUD with react 4

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,12 +3,14 @@ import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManage.scss";
 import { getAllUsers } from "../../services/userService";
+import ModalUser from "./ModalUser";
 
 class UserManage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             arrUsers: [],
+            isOpenModalUser: false,
         };
     }
 
@@ -21,11 +23,37 @@ class UserManage extends Component {
         }
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        });
+    };
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        });
+    };
+
     render() {
         let arrUsers = this.state.arrUsers;
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    test="abc"
+                />
                 <div className="title text-center">Manage Users</div>
+                <div className="mx-1">
+                    <button
+                        className="btn btn-primary px-3 "
+                        onClick={() => this.handleAddNewUser()}
+                    >
+                        <i className="fas fa-plus"></i>
+                        Add new users
+                    </button>
+                </div>
                 <div className="users-table mt-3 mx-1">
                     <table id="customers">
                         <tr>
