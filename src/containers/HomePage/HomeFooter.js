c2f4fda0b3be59d41af1f8f88b3d951e7712ced0
hoaitrinh <hoaitrinh2106@gmail.com>
2024-03-24 13:13:24 +0700

    done home page

---
+++ src/containers/HomePage/HomeFooter.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
+import Slider from "react-slick";
+
+class HomeFooter extends Component {
+    render() {
+        return (
+            <div className="home-footer">
+                <p>
+                    &copy; 2024 BookingCare.com More information, please access
+                    my youtube chanel.
+                    <a
+                        href="https://www.youtube.com/watch?v=147SkAVXEqM&list=PLncHg6Kn2JT6E38Z3kit9Hnif1xC_9VqI&index=62"
+                        target="_blank"
+                    >
+                        &#8594; Click here &#8592;
+                    </a>
+                </p>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeFooter);
