efbd6aad8bca121ddc443a9cb93ad1b63048d3a2
hoaitrinh <hoaitrinh2106@gmail.com>
2024-05-28 18:53:56 +0700

    done manage patient

--- src/containers/System/Doctor/ManagePatient.js
+++ src/containers/System/Doctor/ManagePatient.js
@@ -4,7 +4,15 @@ import "./ManagePatient.scss";
 import DatePicker from "../../../components/Input/DatePicker";
 import { FormattedMessage } from "react-intl";
 import moment from "moment";
-import { getAllPatientForDoctor } from "../../../services/userService";
+import {
+    getAllPatientForDoctor,
+    postSendRemedy,
+} from "../../../services/userService";
+import { LANGUAGE } from "../../../utils";
+import RemedyModal from "./RemedyModal";
+import { toast } from "react-toastify";
+import LoadingOverlay from "react-loading-overlay";
+import { times } from "lodash";
 
 class ManagePatient extends Component {
     constructor(props) {
@@ -13,16 +21,20 @@ class ManagePatient extends Component {
         this.state = {
             currentDate: moment(new Date()).startOf("day").valueOf(),
             dataPatient: [],
+            isOpenRemedyModal: false,
+            dataModal: {},
+            isShowLoading: false,
         };
     }
     async componentDidMount() {
+        this.getDataPatient();
+    }
+
+    getDataPatient = async () => {
         let { user } = this.props;
         let { currentDate } = this.state;
         let formattedDate = new Date(currentDate).getTime();
-        this.getDataPatient(user, formattedDate);
-    }
 
-    getDataPatient = async (user, formattedDate) => {
         let res = await getAllPatientForDoctor({
             doctorId: user.id,
             date: formattedDate,
@@ -45,106 +57,176 @@ class ManagePatient extends Component {
             {
                 currentDate: date[0],
             },
-            () => {
-                let { user } = this.props;
-                let { currentDate } = this.state;
-                let formattedDate = new Date(currentDate).getTime();
-                this.getDataPatient(user, formattedDate);
+            async () => {
+                await this.getDataPatient();
             }
         );
     };
 
-    handleBtnConfirm = () => {};
+    handleBtnConfirm = (item) => {
+        let data = {
+            doctorId: item.doctorId,
+            patientId: item.patientId,
+            email: item.patientData.email,
+            timeType: item.timeType,
+            patientName: item.patientData.firstName,
+        };
+        console.log(data);
+        this.setState({
+            isOpenRemedyModal: true,
+            dataModal: data,
+        });
+    };
 
-    handleBtnRemedy = () => {};
+    closeRemedyModal = () => {
+        this.setState({
+            isOpenRemedyModal: false,
+            dataModal: {},
+        });
+    };
+
+    sendRemedy = async (dataChild) => {
+        let { dataModal } = this.state;
+        this.setState({
+            isShowLoading: true,
+        });
+
+        let res = await postSendRemedy({
+            email: dataChild.email,
+            imgBase64: dataChild.imgBase64,
+            doctorId: dataModal.doctorId,
+            patientId: dataModal.patientId,
+            timeType: dataModal.timeType,
+            language: this.props.language,
+            patientName: dataModal.patientName,
+        });
+        console.log(res);
+
+        if (res && res.errCode === 0) {
+            this.setState({
+                isShowLoading: false,
+            });
+            toast.success("Send Remedy succeed!");
+            this.closeRemedyModal();
+            await this.getDataPatient();
+        } else {
+            this.setState({
+                isShowLoading: false,
+            });
+            toast.error("Something wrongs...");
+            console.log("error send remedy: ", res);
+        }
+    };
 
     render() {
-        let { dataPatient } = this.state;
-        console.log("check patient: ", this.state);
+        let { dataPatient, isOpenRemedyModal, dataModal } = this.state;
+        let { language } = this.props;
         return (
             <>
-                <div className="manage-patient-container">
-                    <div className="m-p-title">Quan ly benh nhan kham benh</div>
-                    <div className="manage-patient-body row">
-                        <div className="col-4 form-group">
-                            <label>Chon ngay kham</label>
-                            <DatePicker
-                                onChange={this.handleOnchangeDatePicker}
-                                className="form-group"
-                                value={this.state.currentDate}
-                            />
+                <LoadingOverlay
+                    active={this.state.isShowLoading}
+                    spinner
+                    text="Loading..."
+                >
+                    <div className="manage-patient-container">
+                        <div className="m-p-title">
+                            Quan ly benh nhan kham benh
                         </div>
-                        <div className="col-12 table-manage-patient">
-                            <table style={{ width: "100%" }}>
-                                <tbody>
-                                    <tr>
-                                        <th>STT</th>
-                                        <th>THOI GIAN</th>
-                                        <th>HO VA TEN</th>
-                                        <th>DIA CHI</th>
-                                        <th>GIOI TINH</th>
-                                        <th>ACTIONS</th>
-                                    </tr>
-                                    {dataPatient && dataPatient.length > 0 ? (
-                                        dataPatient.map((item, index) => {
-                                            return (
-                                                <tr key={index}>
-                                                    <td>{index + 1}</td>
-                                                    <td>
-                                                        {
-                                                            item
-                                                                .timeTypeDataPatient
-                                                                .valueVi
-                                                        }
-                                                    </td>
-                                                    <td>
-                                                        {
-                                                            item.patientData
-                                                                .firstName
-                                                        }
-                                                    </td>
-                                                    <td>
-                                                        {
-                                                            item.patientData
-                                                                .address
-                                                        }
-                                                    </td>
-                                                    <td>
-                                                        {
-                                                            item.patientData
-                                                                .genderData
-                                                                .valueVi
-                                                        }
-                                                    </td>
-                                                    <td>
-                                                        <button
-                                                            className="mp-btn-confirm"
-                                                            onClick={() =>
-                                                                this.handleBtnConfirm()
+                        <div className="manage-patient-body row">
+                            <div className="col-4 form-group">
+                                <label>Chon ngay kham</label>
+                                <DatePicker
+                                    onChange={this.handleOnchangeDatePicker}
+                                    className="form-group"
+                                    value={this.state.currentDate}
+                                />
+                            </div>
+                            <div className="col-12 table-manage-patient">
+                                <table style={{ width: "100%" }}>
+                                    <tbody>
+                                        <tr>
+                                            <th>STT</th>
+                                            <th>THOI GIAN</th>
+                                            <th>HO VA TEN</th>
+                                            <th>DIA CHI</th>
+                                            <th>GIOI TINH</th>
+                                            <th>ACTIONS</th>
+                                        </tr>
+                                        {dataPatient &&
+                                        dataPatient.length > 0 ? (
+                                            dataPatient.map((item, index) => {
+                                                let time =
+                                                    language === LANGUAGE.VI
+                                                        ? item
+                                                              .timeTypeDataPatient
+                                                              .valueVi
+                                                        : item
+                                                              .timeTypeDataPatient
+                                                              .valueEn;
+                                                let gender =
+                                                    language === LANGUAGE.VI
+                                                        ? item.patientData
+                                                              .genderData
+                                                              .valueVi
+                                                        : item.patientData
+                                                              .genderData
+                                                              .valueEn;
+                                                return (
+                                                    <tr key={index}>
+                                                        <td>{index + 1}</td>
+                                                        <td>{time}</td>
+                                                        <td>
+                                                            {
+                                                                item.patientData
+                                                                    .firstName
                                                             }
-                                                        >
-                                                            XAC NHAN
-                                                        </button>
-                                                        <button
-                                                            className="mp-btn-remedy"
-                                                            onClick={() =>
-                                                                this.handleBtnRemedy()
+                                                        </td>
+                                                        <td>
+                                                            {
+                                                                item.patientData
+                                                                    .address
                                                             }
-                                                        >
-                                                            GUI HOA DON
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                            );
-                                        })
-                                    ) : (
-                                        <tr>NO DATA</tr>
-                                    )}
-                                </tbody>
-                            </table>
+                                                        </td>
+                                                        <td>{gender}</td>
+                                                        <td>
+                                                            <button
+                                                                className="mp-btn-confirm"
+                                                                onClick={() =>
+                                                                    this.handleBtnConfirm(
+                                                                        item
+                                                                    )
+                                                                }
+                                                            >
+                                                                Xac nhan
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                );
+                                            })
+                                        ) : (
+                                            <tr>
+                                                <td
+                                                    colSpan="6"
+                                                    style={{
+                                                        textAlign: "center",
+                                                    }}
+                                                >
+                                                    No data
+                                                </td>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    <RemedyModal
+                        isOpenModal={isOpenRemedyModal}
+                        dataModal={dataModal}
+                        closeRemedyModal={this.closeRemedyModal}
+                        sendRemedy={this.sendRemedy}
+                    />
+                </LoadingOverlay>
             </>
         );
     }
---
+++ src/containers/System/Doctor/RemedyModal.js
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./RemedyModal.scss";
+import { FormattedMessage } from "react-intl";
+import { toast } from "react-toastify";
+import moment from "moment";
+import { CommonUtils } from "../../../utils";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { every } from "lodash";
+
+class RemedyModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            imgBase64: "",
+        };
+    }
+
+    async componentDidMount() {
+        if (this.props.dataModal) {
+            this.setState({
+                email: this.props.dataModal.email,
+            });
+        }
+    }
+
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.dataModal !== prevProps.dataModal) {
+            this.setState({
+                email: this.props.dataModal.email,
+            });
+        }
+    }
+
+    handleOnchangeEmail = (event) => {
+        this.setState({
+            email: event.target.value,
+        });
+    };
+
+    handleOnchangeImage = async (event) => {
+        let data = event.target.files;
+        let file = data[0];
+        if (file) {
+            let base64 = await CommonUtils.getBase64(file);
+            this.setState({
+                imgBase64: base64,
+            });
+        }
+    };
+
+    handleSendRemedy = () => {
+        this.props.sendRemedy(this.state);
+    };
+
+    render() {
+        let { isOpenModal, closeRemedyModal, dataModal, sendRemedy } =
+            this.props;
+        return (
+            <Modal
+                isOpen={isOpenModal}
+                className="remedy-modal-container"
+                size="lg"
+                centered
+                // backdrop={true}
+            >
+                <div className="modal-header">
+                    <h5 className="modal-title">Gui hoa don thanh cong</h5>
+                    <button
+                        type="button"
+                        className="close"
+                        aria-label="Close"
+                        onClick={closeRemedyModal}
+                    >
+                        <span aria-hidden="true">x</span>
+                    </button>
+                </div>
+
+                <ModalBody>
+                    <div className="row">
+                        <div className="col-6 form-group">
+                            <label>Email benh nhan</label>
+                            <input
+                                className="form-control"
+                                type="email"
+                                value={this.state.email}
+                                onChange={(event) => {
+                                    this.handleOnchangeEmail(event);
+                                }}
+                            ></input>
+                        </div>
+
+                        <div className="col-6 form-group">
+                            <label>Chon file don thuoc</label>
+                            <input
+                                className="form-control-file"
+                                type="file"
+                                onChange={(event) => {
+                                    this.handleOnchangeImage(event);
+                                }}
+                            ></input>
+                        </div>
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <Button
+                        color="primary"
+                        onClick={() => this.handleSendRemedy()}
+                    >
+                        Send
+                    </Button>
+                    <Button color="secondary" onClick={closeRemedyModal}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RemedyModal);
